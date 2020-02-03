e8e5cb4cf6646d901b62017879001fe9fc9b384b
Vitor Marques Costa <vmarquec@everis.com>
2020-02-03 10:02:42 -0300

    Referenciando os posts do usuário

--- src/main/java/com/example/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/example/workshopmongo/dto/AuthorDTO.java
@@ -7,16 +7,24 @@ import com.example.workshopmongo.domain.User;
 public class AuthorDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private String name;
-	private String email;	
+	private String id;
+	private String name;	
 	
 	public AuthorDTO() {
 		
 	}
 	
 	public AuthorDTO(User obj) {
+		id = obj.getId();
 		name = obj.getName();
-		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -25,14 +33,6 @@ public class AuthorDTO implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 	
 	
